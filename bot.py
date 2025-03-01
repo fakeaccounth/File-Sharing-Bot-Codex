452c49ad4939830298418c06650f0a11889ee7ea
rohit3607 <rohitplayer87089@gmail.com>
2025-03-01 22:16:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,7 +66,6 @@ class Bot(Client):
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
-        print(ascii_art)
         print("""Welcome to CodeXBotz File Sharing Bot""")
         self.username = usr_bot_me.username
         #web-response
