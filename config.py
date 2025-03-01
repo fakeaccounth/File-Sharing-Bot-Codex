553f842a0e44bbac6fc13ac5e8afa195ed899abf
rohit3607 <rohitplayer87089@gmail.com>
2025-03-01 22:13:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,12 +11,12 @@ from logging.handlers import RotatingFileHandler
 
 #Bot token @Botfather
 
-BOT_TOKENS = os.environ.get("BOT_TOKENS", "6403720726:AAH2s38VIkj9TWcxA2ZNlRmnz-G2CSot4MA,7934694179:AAGoY4YqNZ-TYq1i3LYohud8a8pTa2LH6HM").split(",")
+#BOT_TOKENS = os.environ.get("BOT_TOKENS", "6403720726:AAH2s38VIkj9TWcxA2ZNlRmnz-G2CSot4MA,7934694179:AAGoY4YqNZ-TYq1i3LYohud8a8pTa2LH6HM").split(",")
 
-if not BOT_TOKENS:
+#if not BOT_TOKENS:
     raise Exception("No bot tokens provided!")
 
-print(BOT_TOKENS)  # Output: ['12345:bot_token1', '67890:bot_token2']
+#print(BOT_TOKENS)  # Output: ['12345:bot_token1', '67890:bot_token2']
 
 
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6403720726:AAH2s38VIkj9TWcxA2ZNlRmnz-G2CSot4MA")
