980db4bcbee7cdccb85d827a7f6d3289c4627c29
rohit3607 <rohitplayer87089@gmail.com>
2025-03-01 23:04:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,10 +56,13 @@ class Bot(Client):
 
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
-            test = await self.send_message(chat_id=db_channel.id, text="Test Message")
+            self.db_channel = db_channel
+            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
             await test.delete()
         except Exception as e:
-            self.LOGGER(__name__).warning(f"DB Channel Error: {e}")
+            self.LOGGER(__name__).warning(e)
+            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
