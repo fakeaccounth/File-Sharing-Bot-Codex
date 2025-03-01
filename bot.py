eee4e2fca169e260a853f119ad3f5450a621596c
rohit3607 <rohitplayer87089@gmail.com>
2025-03-01 21:06:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-import asyncio
+#(©)Codexbotz
+
 from aiohttp import web
 from plugins import web_server
 
@@ -11,18 +12,28 @@ from datetime import datetime
 from config import *
 
 
+ascii_art = """
+░█████╗░░█████╗░██████╗░███████╗██╗░░██╗██████╗░░█████╗░████████╗███████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗╚══██╔══╝╚════██║
+██║░░╚═╝██║░░██║██║░░██║█████╗░░░╚███╔╝░██████╦╝██║░░██║░░░██║░░░░░███╔═╝
+██║░░██╗██║░░██║██║░░██║██╔══╝░░░██╔██╗░██╔══██╗██║░░██║░░░██║░░░██╔══╝░░
+╚█████╔╝╚█████╔╝██████╔╝███████╗██╔╝╚██╗██████╦╝╚█████╔╝░░░██║░░░███████╗
+░╚════╝░░╚════╝░╚═════╝░╚══════╝╚═╝░░╚═╝╚═════╝░░╚════╝░░░░╚═╝░░░╚══════╝
+"""
+
 class Bot(Client):
-    def __init__(self, bot_token):
+    def __init__(self):
         super().__init__(
-            name=f"Bot-{bot_token[:5]}",  # Unique name for each bot instance
+            name="Bot",
             api_hash=API_HASH,
             api_id=APP_ID,
-            plugins={"root": "plugins"},
+            plugins={
+                "root": "plugins"
+            },
             workers=TG_BOT_WORKERS,
-            bot_token=bot_token,
+            bot_token=TG_BOT_TOKEN
         )
         self.LOGGER = LOGGER
-        self.bot_token = bot_token
 
     async def start(self):
         await super().start()
@@ -37,39 +48,33 @@ class Bot(Client):
                     link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
                 self.invitelink = link
             except Exception as a:
-                self.LOGGER(__name__).warning(f"[{self.bot_token[:5]}] {a}")
-                self.LOGGER(__name__).warning(f"[{self.bot_token[:5]}] Bot can't Export Invite link from Force Sub Channel!")
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
+                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
                 sys.exit()
-
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id=db_channel.id, text="Test Message")
+            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
             await test.delete()
         except Exception as e:
-            self.LOGGER(__name__).warning(f"[{self.bot_token[:5]}] {e}")
+            self.LOGGER(__name__).warning(e)
+            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"[{self.bot_token[:5]}] Bot Running..!\n")
-
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
+        print(ascii_art)
+        print("""Welcome to CodeXBotz File Sharing Bot""")
         self.username = usr_bot_me.username
-        # Web Server
+        #web-response
         app = web.AppRunner(await web_server())
         await app.setup()
-        await web.TCPSite(app, "0.0.0.0", PORT).start()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
 
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info(f"[{self.bot_token[:5]}] Bot stopped.")
-
-
-async def main():
-    bot1 = Bot(TG_BOT_TOKEN_1)
-    bot2 = Bot(TG_BOT_TOKEN_2)
-
-    await asyncio.gather(bot1.start(), bot2.start())
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+        self.LOGGER(__name__).info("Bot stopped.")
