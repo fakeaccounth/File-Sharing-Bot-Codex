70dd3a7ad820bcae92033d9c25e7547391333b80
rohit3607 <rohitplayer87089@gmail.com>
2025-03-01 22:22:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,8 +57,8 @@ class Bot(Client):
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id=db_channel.id, text="Test Message")
-            await test.delete()
+            #test = await self.send_message(chat_id=db_channel.id, text="Test Message")
+            #await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(f"Database channel error: {e}")
             sys.exit()
