c18c5fc91a40470bfa251319ee6fcab1ef5c971a
rohit3607 <rohitplayer87089@gmail.com>
2025-03-01 20:59:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,10 +31,10 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", " -1001999588896"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "5897568676"))
 
 #Port
-PORT = os.environ.get("PORT", "8080")
+PORT = os.environ.get("PORT", "3728")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://MONGONOOB2:MONGONOOB2@cluster0.7eyuns5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://MONGONOOB2:MONGONOOB2@cluster0.7eyuns5.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "MONGONOOB2")
 
 #force sub channel id, if you want enable force sub
