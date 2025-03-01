88f3e1c1ac6e84a59e0fa4785026c0066db884a8
noobnoobita <127819990+noonnoobita@users.noreply.github.com>
2025-03-01 20:56:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,32 +10,32 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN_1 = os.environ.get("TG_BOT_TOKEN_1", "")
+TG_BOT_TOKEN_1 = os.environ.get("TG_BOT_TOKEN_1", "6403720726:AAH2s38VIkj9TWcxA2ZNlRmnz-G2CSot4MA")
 
 
-TG_BOT_TOKEN_2 = os.environ.get("TG_BOT_TOKEN_2", "")
+TG_BOT_TOKEN_2 = os.environ.get("TG_BOT_TOKEN_2", "7934694179:AAGoY4YqNZ-TYq1i3LYohud8a8pTa2LH6HM")
 
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
 #bot username for file retrieval | without @
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "29203773"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "955f05c16722e99e157125fc025bac41")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", " -1001999588896"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "5897568676"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://MONGONOOB2:MONGONOOB2@cluster0.7eyuns5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "MONGONOOB2")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
