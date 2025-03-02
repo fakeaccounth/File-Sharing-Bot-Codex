5743e9ba7bbc741a7b0743016308df06033cd3e8
noobnoobita <127819990+noonnoobita@users.noreply.github.com>
2025-03-02 10:00:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-import asyncio
-from bot import Bot, Dot
+from bot import Bot
 
-bot1 = Bot()
-bot2 = Dot()
-
-async def run_bots():
-    await asyncio.gather(bot1.start(), bot2.start())
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run_bots())
-    loop.run_forever()
+Bot().run()
