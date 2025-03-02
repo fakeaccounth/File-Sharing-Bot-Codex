f0e464ad7e8c1d0eafd8bfcca8f7c4a4955d61c3
rohit3607 <rohitplayer87089@gmail.com>
2025-03-02 09:03:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ class Bot(Client):
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
             test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
-            await test.delete()
+            #await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
@@ -114,7 +114,7 @@ class Dot(Client):
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
             test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
-            await test.delete()
+            #await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
