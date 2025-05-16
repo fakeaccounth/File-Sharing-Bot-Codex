a9d57cbe2990c61f0c5130cf78c290f979e8d54b
fakeaccounth <sumitkbdlitani123@gmail.com>
2025-05-16 20:43:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,8 +56,8 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 
 # Auto delete time in seconds.
 AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "130000"))
-AUTO_DELETE_MSG = os.environ.get("AUTO_DELETE_MSG", "File will be automatically deleted in 35 Hours. Please Watch Before It Will Delete.")
-AUTO_DEL_SUCCESS_MSG = os.environ.get("AUTO_DEL_SUCCESS_MSG", "📂 Everthing Is Gone As We Said 🔥")
+AUTO_DELETE_MSG = os.environ.get("AUTO_DELETE_MSG", "<b>📂 Will be Deleted in 35 Hours. Watch Before It Will Delete.</b>")
+AUTO_DEL_SUCCESS_MSG = os.environ.get("AUTO_DEL_SUCCESS_MSG", "<b>📂 Deleted Successfully ✅</b>")
 
 #Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
